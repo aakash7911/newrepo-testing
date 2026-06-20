37a290aa29ef0eb8d6097aeda53d854f0bc5dc85
aakash <dr282924@gmail.com>
2026-06-20 22:10:51 +0530

    chore: revert SECRET_KEY back to hardcoded value

--- script1.js
+++ script1.js
@@ -3056,7 +3056,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-const SECRET_KEY = window.APP_VAULT_SECRET;
+const SECRET_KEY = "Zobbly_Secure_Lock_99";
 
 
 function encrypt(data) {
